e8f42dff9725dcb6e5ef45c26f8f0fe7d8c5b2a5
havuong <havuong138@gmail.com>
2018-12-20 22:26:04 +0700

    update TC SignIn and CommonFunction

--- src/main/java/test/modules/SignIn.java
+++ src/main/java/test/modules/SignIn.java
@@ -1,31 +1,49 @@
 package test.modules;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.How;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static test.supports.CommonFunctions.*;
 
 public class SignIn {
+
+    @BeforeClass
+    public void Before() {
+        System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://testapp.galenframework.com/");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
     @Test
-    public void signIn(){
-        setBrowser("chr");
-        visit("http://testapp.galenframework.com/");
-        click(How.XPATH,"//button[text()='Login']");
+    public void TC01_SignIn() throws InterruptedException {
 
-        click(How.XPATH,"//input[@name='login.username']");
-        click(How.XPATH,"//input[@name='login.password']");
-        fill(How.NAME,"login.username","testuser@example.com");
-        fill(How.NAME,"login.password","test123");
+        click(How.XPATH, "//button[text()='Login']");
 
-        click(How.XPATH,"//button[text()='Login']");
+        fill(How.NAME, "login.username", "testuser@example.com");
+        fill(How.NAME, "login.password", "test123");
 
-        click(How.XPATH,"//button[text()='Add note']");
+        click(How.XPATH, "//button[text()='Login']");
+}
 
-        fill(How.NAME,"note.title","test");
-        fill(How.NAME,"note.description","add new note");
+    @Test
+    public void TC02_AddNote() throws InterruptedException {
+        click(How.XPATH, "//button[text()='Add note']");
+        fill(How.NAME, "note.title", "test");
+        fill(How.NAME, "note.description", "add new note");
 
-        click(How.XPATH,"//button[text()='Add Note']");
+        click(How.XPATH, "//button[text()='Add Note']");
+    }
 
 
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
     }
 }
--- src/main/java/test/supports/CommonFunctions.java
+++ src/main/java/test/supports/CommonFunctions.java
@@ -1,15 +1,12 @@
 package test.supports;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,7 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class CommonFunctions {
     public static WebDriver driver;
 
-    public static WebElement getElement( How how, String locator ){
+    public static WebElement getElement(How how, String locator) {
         switch (how) {
             case CLASS_NAME:
                 return driver.findElement(By.className(locator));
@@ -43,8 +40,8 @@ public class CommonFunctions {
         return null;
     }
 
-    public static void setBrowser(String browserName){
-        switch (browserName){
+    public static void setBrowser(String browserName) {
+        switch (browserName) {
             case "ff":
                 driver = new FirefoxDriver();
                 break;
@@ -57,63 +54,30 @@ public class CommonFunctions {
             case "edge":
                 driver = new EdgeDriver();
                 break;
-            case "cc":
-                System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
-                ChromeOptions options = new ChromeOptions();
-                options.setBinary("C:/Users/BillGates/AppData/Local/CocCoc/Browser/Application/browser.exe");
-                driver = new ChromeDriver(options);
-                break;
         }
-
     }
 
-    public static void visit(String url){
+    public static void visit(String url) {
         driver.get(url);
     }
-    public static void closeBrowser(){
+
+    public static void closeBrowser() {
         driver.quit();
     }
-    public static void waitForElement(How how,String locator,int timeout){
-        new WebDriverWait(driver,timeout).until(ExpectedConditions.visibilityOf(getElement(how, locator)));
+
+    public static void waitForElement(How how, String locator, int timeout) {
+        new WebDriverWait(driver, timeout).until(ExpectedConditions.visibilityOf(getElement(how, locator)));
     }
-    public static void click(How how, String locator){
-        waitForElement(how,locator,30);
+
+    public static void click(How how, String locator) {
+        waitForElement(how, locator, 30);
         getElement(how, locator).click();
     }
-    public static void dblClick (How how, String locator) {
-        Actions actions = new Actions(driver);
-        waitForElement(how,locator,30);
-        actions.doubleClick(getElement(how,locator)).perform();
-    }
-    public static void fill(How how,String locator,String withText){
-        waitForElement(how,locator,30);
+
+    public static void fill(How how, String locator, String withText) {
+        waitForElement(how, locator, 30);
         getElement(how, locator).clear();
         getElement(how, locator).sendKeys(withText);
-    }
-    public static void fillKeys(How how,String locator,Keys withKey){
-        waitForElement(how,locator,30);
-        getElement(how, locator).clear();
-        getElement(how, locator).sendKeys(withKey);
-    }
-    public static String getText(How how, String locator){
-        waitForElement(how,locator,30);
-        return getElement(how, locator).getText();
-    }
-    public static String getAttri(How how,String locator,String attribute){
-        waitForElement(how,locator,30);
-        return getElement(how, locator).getAttribute(attribute);
-    }
-    public static String getCSSValue(How how,String locator,String CSSValue){
-        waitForElement(how,locator,30);
-        return getElement(how, locator).getCssValue(CSSValue);
-    }
-    public static boolean isElementDisplayed(How how,String locator){
-        waitForElement(how,locator,30);
-        return getElement(how, locator).isDisplayed();
-    }
-    public static boolean isElementSelected(How how,String locator){
-        waitForElement(how,locator,30);
-        return getElement(how, locator).isSelected();
     }
 }
 
